349465df4d1633862aa62b44f7179e020f5eb311
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-03-27 14:36:13 +0100

    fix: Fix app management

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ from auth import AuthError, requires_auth
 
 PAGE_LENGTH = 10
 
-app = Flask(__name__)
-
 
 def create_app(test_config=None):
+    app = Flask(__name__)
     setup_db(app)
 
     CORS(app)
