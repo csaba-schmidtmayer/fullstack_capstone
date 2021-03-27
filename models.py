e3dd5179fdfcf12d9d39adba01ce180412dbc815
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-03-27 14:45:51 +0100

    fix: Fix app management

--- models.py
+++ models.py
@@ -26,7 +26,6 @@ def setup_db(app, database_path=DB_PATH):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
 
 
 class Movie(db.Model):
